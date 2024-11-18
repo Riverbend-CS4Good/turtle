8566b37e91b9b9d1372c73ee82033483949b8369
Aaron Wang <aaronwang04@icloud.com>
2024-11-17 20:47:28 -0500

    idk

---
+++ commands/running_functions.js
@@ -0,0 +1,7 @@
+const resetBtn = document.getElementById('reset');
+const runBtn = document.getElementById('run');
+resetBtn.addEventListener('click', reset);
+
+function reset() {
+    resize();
+}
