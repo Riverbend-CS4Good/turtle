d896721a43405053cd0b388ebba682799cb44703
cht-au <145236495+cht-au@users.noreply.github.com>
2024-12-05 20:34:26 -0500

    implement fast forward button

--- running_program.js
+++ running_program.js
@@ -1,9 +1,19 @@
-let runButtonClass = document.getElementsByClassName('run-button');
-const button = runButtonClass[0]
+const button = document.getElementsByClassName('run-button')[0];
+const forwardButton = document.getElementsByClassName('foward-button')[0];
+
 let isPaused = true; // Initial state is paused
 let lines = []; // Array to hold each line of the file
 let currentLine = 0;
 let intervalID = null;
+
+forwardButton.addEventListener("click", () => {
+    clearInterval(intervalID);
+    for(let i = currentLine; i < lines.length; i++) {
+        console.log(lines[i]);
+    }
+    currentLine = lines.length;
+    isPaused = true;
+});
 
 button.addEventListener("click", () => {
     if (isPaused) {
