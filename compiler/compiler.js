a0f6db4f62408f75af0eb114c78a1fd9cf54aca6
tealeaf2 <shadowrocket2003@gmail.com>
2025-01-08 15:18:58 -0600

    WIP

---
+++ compiler/compiler.js
@@ -0,0 +1,4 @@
+// TODO
+// Lexer - takes inputs and creates tokens
+// Parser - takes tokens and produces an abstract syntax tree
+// Interpreter - uses the ast and interprets it on the fly, line per line
