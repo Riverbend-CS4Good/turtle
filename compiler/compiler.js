e52e89d721390c3197707373432e4ce53db0372e
tealeaf2 <shadowrocket2003@gmail.com>
2025-01-20 14:29:10 -0500

    WIP

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -2,3 +2,12 @@
 // Lexer - takes inputs and creates tokens
 // Parser - takes tokens and produces an abstract syntax tree
 // Interpreter - uses the ast and interprets it on the fly, line per line
+
+// inputs for testing
+
+let turtle_canvas = document.getElementById('turtle');
+let sandbox_canvas = document.getElementById('sandbox');
+let turtle_ctx = turtle_canvas.getContext('2d');
+let sandbox_ctx = sandbox_canvas.getContext('2d');
+let inner = document.getElementById('inner');
+
