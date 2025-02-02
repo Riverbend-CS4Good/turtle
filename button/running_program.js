a2f49756dae25a7a226c1bcb8d8f10394f1442cf
cht-au <145236495+cht-au@users.noreply.github.com>
2025-02-01 21:25:58 -0500

    move content of running_program and fix fast forward button

--- button/running_program.js
+++ button/running_program.js
@@ -4,13 +4,15 @@ const forwardButton = document.getElementById('forward-button');
 
 let isPaused = true; // Initial state is paused
 let lines = []; // Array to hold each line of the file
+const fileInput = document.getElementById('file-input');
+const input = document.getElementById('editor');
 let currentLine = 0;
 let intervalID = null;
 
 forwardButton.addEventListener("click", () => {
     clearInterval(intervalID);
     for(let i = currentLine; i < lines.length; i++) {
-        console.log(lines[i]);
+        printLine()
     }
     currentLine = lines.length;
     isPaused = true;
@@ -19,33 +21,44 @@ forwardButton.addEventListener("click", () => {
 runButton.addEventListener("click", () => {
     if (isPaused) {
         isPaused = false;
-        button.textContent = "Pause";
+        runButton.textContent = "Pause";
         readNext()
     } else {
         isPaused = true;
-        button.textContent = "Run";
+        runButton.textContent = "Run";
         clearInterval(intervalID)
     }
 });
 
+saveButton.addEventListener("click", () => {
+    var text = input.value;
+    fileInput.value = '';
+    displayContents(text);
+    saveButton.textContent = "Saved";
+});
+
+input.addEventListener('input', () => {
+    saveButton.textContent = "Save";
+})
+
 function readSingleFile(e) {
     var file = e.target.files[0];
     if (!file) {
-      return;
+        return;
     }
     var reader = new FileReader();
-    reader.onload = function(e) {
-      var contents = e.target.result;
-      displayContents(contents);
+    reader.onload = function (e) {
+        var contents = e.target.result;
+        displayContents(contents);
     };
     reader.readAsText(file);
-  }
-  
-  function displayContents(contents) {
+}
+
+function displayContents(contents) {
     lines = contents.split("\n");
-  }
-  
-  document.getElementById('file-input')
+}
+
+document.getElementById('file-input')
     .addEventListener('change', readSingleFile, false);
 
 
@@ -58,14 +71,17 @@ function readNext() {
 }
 
 function printLine() {
-    if(currentLine < lines.length && !isPaused) {
+    if (currentLine < lines.length && !isPaused) {
         console.log(lines[currentLine]);
+        let args = lines[currentLine].split(" ");
+        action(args[0], Number(args[1]));
         currentLine++;
     }
     else {
         clearInterval(intervalID)
-        if(currentLine >= lines.length) {
-            button.textContent = "Finished";
+        if (currentLine >= lines.length) {
+            runButton.textContent = "Finished";
+            currentLine = 0;
         }
     }
 }
